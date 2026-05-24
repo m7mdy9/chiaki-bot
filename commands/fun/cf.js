17cec92e2841cb1f4b134173f035df55c15505b0
m7mdy9 <hamestar1122@gmail.com>
2026-05-24 22:08:54 +0300

    Updated the cf.js command to include embeds, avoid errors and collisions with Discord REST API

--- commands/fun/cf.js
+++ commands/fun/cf.js
@@ -1,23 +1,33 @@
-const { getOptionNum } = require("../../utils/utils.js")
+const { getOptionNum, embed_builder } = require("../../utils/utils.js")
 
 module.exports = {
     name:"coinflip",
     description:"Flips a coin!",
     options: [
         {
-            name:'N',
+            name:'flips',
             description: 'Number of times to flip the coin.',
             type: getOptionNum("INTEGER"),
             required: false,
         },
     ],
-
+    /**
+     * @param {import('discord.js').ChatInputCommandInteraction} interaction 
+     */
     async execute(interaction){
-        const numFlips = interaction.options.getInteger('N')
-        let results = []
-        for (let i = 0; i < numFlips; i++) {
-            results.push(Math.random() >= 0.5 ? "Heads" : "Tails")
+        const numFlips = interaction.options.getInteger('flips') || null
+        let output;
+        if (numFlips && numFlips != 1){
+            let results = []
+            for (let i = 0; i < numFlips; i++) {    
+                results.push(Math.random() >= 0.5 ? "Heads" : "Tails")
+            }
+            output = `🪙 Flipped a coin **${numFlips}** times: **${results.join(", ")}**`
+        } else  {
+            output = `🪙 **${Math.random() >= 0.5 ? "Heads" : "Tails"}**`
         }
-        await interaction.editReply({ content: `Flipped a coin ${numFlips} times: ${results.join(", ")}` })
+        await interaction.editReply({ embeds:[
+            embed_builder("Result",output).setTimestamp()
+        ]})
     }
 }
