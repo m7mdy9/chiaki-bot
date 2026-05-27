5875b893e7ced251357fb0e0e98afbbecda258f0
m7mdy9 <hamestar1122@gmail.com>
2026-05-27 09:03:48 +0300

    Added /random chiaki and included some chiaki quotes in config.json

---
+++ commands/fun/random/chiaki.js
@@ -0,0 +1,19 @@
+const { embed_builder } = require("../../../utils/utils.js")
+const { chiaki_half_body_sprites, chiaki_quotes } = require("../../../utils/config.json")
+
+module.exports = {
+    name: "chiaki",
+    description: "Get a random chiaki sprite and line!",
+    /** @param {import("discord.js").ChatInputCommandInteraction} interaction */
+    async execute(interaction){
+        const site = `https://m7mdy9.github.io/images/chiaki_half_body_sprites/`
+        const randomChiaki = chiaki_half_body_sprites[Math.floor(Math.random() * chiaki_half_body_sprites.length)]
+        const randomQuote = chiaki_quotes[Math.floor(Math.random() * chiaki_quotes.length)]
+        const ChiakiSprite = `${site}${randomChiaki}`
+
+        const embed = embed_builder("Random Chiaki Image!",
+        `"${randomQuote}" **- Chiaki Nanami**`
+        ).setImage(ChiakiSprite).setFooter({text:"Images from danganronpa.fandom.com"})
+        return interaction.editReply({embeds:[embed]})
+    }
+}
