b607387b4d7b9386f65260a2cdaa5c14e1882fe8
m7mdy9 <hamestar1122@gmail.com>
2026-06-09 13:58:41 +0300

    Fixed execute.js by adding a targetMember and targetUser to avoid errors while calling .username, also added comments inside execute.js to explain the process.

--- commands/fun/execute.js
+++ commands/fun/execute.js
@@ -23,23 +23,41 @@ module.exports = {
      */
     async execute(interaction){
         try{
-            const targetUser = interaction.options.getMember("student") || interaction.options.getUser("student") || interaction.user
-            const avatarPath = targetUser.displayAvatarURL({size:128});
+            // trying to get member if not we get user, if neither we get the author of the interaction (for displayAvatarURL)
+            const targetMember = interaction.options.getMember("student") || interaction.options.getUser("student") || interaction.user
+            // we just get the user if not we get the interaction author, this is for fetching the username, .username wont work with member
+            const targetUser = interaction.options.getUser("student") || interaction.user
+            
+            // fetching display avatar URL from either the member, user or the author
+            const avatarPath = targetMember.displayAvatarURL({size:128});
+            // username of the user or author
             const username = targetUser.username
             
+            // measuring how long it has been since the process started
             const startTime = performance.now()
+
+            // running our gifWorker.js as a threaded worker to avoid blocking and performance drops and it returns a Uint8Array Buffer
             const gifBuffer = await worker.run({avatarPath, username})
+
+            // transfers the gifBuffer into the Buffer class so discord actually doesnt break the gif!
             const formattedGifBuffer = Buffer.from(gifBuffer)
             console.log(formattedGifBuffer)
+
+            // creating the gif attachment that will be sent in discord
             const gifAttachment = new AttachmentBuilder(formattedGifBuffer, { name: 'execute-avatar.gif'})
+            
+            // measuring how long it took for the process in seconds and allowing 2 decimal points 
             const timeTakenToExecute = ((performance.now() - startTime)/1000).toFixed(2)
+            
+            // sending the gif with the timeTakenToExecute
             return interaction.editReply({
                 files: [gifAttachment],
                 content:`-# Generated in ${timeTakenToExecute}s`
             })
         } catch(err){
+            // catches the error to avoid crashing 
             interaction.editReply("Could not generate an execution gif.")
-            console.error(`Error in ${__filename}: `,err)
+            return console.error(`Error in ${__filename}: `,err)
         }
     }
 }
