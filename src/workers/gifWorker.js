ff863ac5f3f06cfb49d1e6316ccc4e7fc79b32e3
m7mdy9 <hamestar1122@gmail.com>
2026-06-20 22:39:15 +0300

    Documented and cleaned gifWorker.js

--- src/workers/gifWorker.js
+++ src/workers/gifWorker.js
@@ -4,45 +4,53 @@ const { createCanvas, GlobalFonts, loadImage } = require("@napi-rs/canvas")
 const gifFrames = require('gif-frames');
 const GIFEncoder = require("gif-encoder-2/src/GIFEncoder");
 
-const imgDir =(frameNum)=>{
-    let result
+/** 
+ * @param {Number} frameIndex - The frame Index/Number
+ * @returns \{xCord, yCord} depending on the frame index
+*/
+const getFrameInfo =(frameIndex)=>{
+    let result = { xCord:0, yCord:0 }
     switch (true) {
-        case frameNum<24+1:
-            result = [205, 90]
+        case frameIndex<24+1:
+            result = { xCord:205, yCord: 90 }
             break;
-        case frameNum<27+1:
-            result = [264, 100]
+        case frameIndex<27+1:
+            result = { xCord:264, yCord: 100 }
             break;
-        case frameNum<30+1:
-            result = [287, 100]
+        case frameIndex<30+1:
+            result = { xCord:287, yCord: 100 }
             break;
-        case frameNum<32+1:
-            result = [306, 100]
+        case frameIndex<32+1:
+            result = { xCord:306, yCord: 100 }
             break;
-        case frameNum<35+1:
-            result = [329, 100]
+        case frameIndex<35+1:
+            result = { xCord:329, yCord: 100 }
             break;
-        case frameNum<38+1:
-            result = [348, 100]
+        case frameIndex<38+1:
+            result = { xCord:348, yCord: 100 }
             break;
-        case frameNum<42+1:
-            result = [369, 100]
+        case frameIndex<42+1:
+            result = { xCord:369, yCord: 100 }
             break;
-        case frameNum<43+1:
-            result = [390, 100]
+        case frameIndex<43+1:
+            result = { xCord:390, yCord: 100 }
             break;
-        case frameNum<50+1:
-            result = [408, 100]
+        case frameIndex<50+1:
+            result = { xCord:408, yCord: 100 }
             break;
     }
     return result
 }
 
+// Loading the Danganronpa Font that will be used in buildEditedImage()
+const executionFontPath = path.join(process.cwd(), "media/pixel-danganronpa.otf")
+GlobalFonts.registerFromPath(executionFontPath, "dang")
+
 const totalFrames = 51
 const width = 498;
 const height = 278;
-// const avatarPath = "https://images-ext-1.discordapp.net/external/VvYGBMGAOxHGqy1QFeJm3u8Nta655M0vsYWDyYGdL8Y/%3Fsize%3D512/https/cdn.discordapp.com/avatars/488132273518936084/671b52cdb5b28cacfa1061899117b77e.webp?format=webp"
 
+// returns a Promise that turns the buffer into a stream 
 function streamToBuffer(stream) {
   return new Promise((resolve, reject) => {
     const chunks = [];
@@ -51,85 +59,145 @@ function streamToBuffer(stream) {
     stream.on('error', (err) => reject(err));
   });
 }
+
+let dissectedGifFrames = null;
+
+/** 
+ * Dissects the gif into frames
+ * 1. If the gif was already dissected, it returns the dissected gif
+ * 2. Otherwise, it calls gifFrames which turns the gif into images of each frame
+ * 3. using .getImage() on each frame returns us a stream, so we turn them into a Buffer using streamToBuffer() function
+ * 4. We load each frame into canvas using loadImage() and into a variable named cachedFrames
+ * 5. We assign the empty dissectedGifFrames variable the cachedFrames and return it.
+*/
 async function dissectGif(){
-    const extractedFrames = await gifFrames({
-        url: path.join(process.cwd(), 'media/execution_dr1_optimized.gif'),
-        frames: 'all',
-        outputType: 'png',
-        cumulative: true,
-    })
-    return extractedFrames    
+    if(dissectedGifFrames) return dissectedGifFrames;
+    else {
+        const extractedFrames = await gifFrames({
+            url: path.join(process.cwd(), 'media/execution_dr1_optimized.gif'),
+            frames: 'all',
+            outputType: 'png',
+            cumulative: true,
+        })
+
+        const frameBuffers = await Promise.all(extractedFrames.map(frame=>streamToBuffer(frame.getImage())))
+
+        const cachedFrames = await Promise.all(frameBuffers.map(buffer => loadImage(buffer)))
+
+        dissectedGifFrames = cachedFrames
+
+        return cachedFrames
+    }
 }
+
+/**
+ * Creates a new frame, with the background set as backgroundImage, avatar set as avatarImage and username is displayed and segmented.
+ *
+ *  - Functions steps:
+ * 
+ * 1. Clearing the ctx, and getting the frame info
+ * 2. Drawing the backgroundImage and the avatarImage at their correct coordiantes
+ * 3. Setting the font/text options for the written out username.
+ * 4. If the username is longer than 19 characters, we slice it and add ".." at the end.
+ * 5. If the frame index is more than 2, we write out the actual username
+ * 6. When writing the username, depending on the current frame, we choose how many characters to display depending on the frameIndex
+ * 7. Then we write the segmentToUse (the characters that will displayed)
+ * 8. Finally, we return the ctx.
+ * @param {import('@napi-rs/canvas').Image} avatarImage - Image that will be plastered on the sprite
+ * @param {import('@napi-rs/canvas').Image} backgroundImage - Image that will cover the background.
+ * @param {import('@napi-rs/canvas').CanvasRenderingContext2D} ctx - ctx of the canvas you have created (width and height must match that of the `backgroundImage`)
+ * @param {Number} frameIndex - The index/number of the current frame. (starting from 0)
+ * @param {String} username - Username of the user that will be animated/displayed inside the image.
+ * @returns {import('@napi-rs/canvas').CanvasRenderingContext2D} Returns the `ctx` provided cleared and with the drawn elements 
+ */
+async function buildEditedImage(ctx, backgroundImage, avatarImage, frameIndex, username){
+
+    ctx.clearRect(0, 0, width, height);
+
+    const cordsArray = getFrameInfo(frameIndex)
+    const { xCord, yCord } = cordsArray
+    const size = 90 // the height/width used in drawing the avatarImage
+    
+    
+    ctx.drawImage(backgroundImage, 0,0, width,height)
+    ctx.drawImage(avatarImage, xCord, yCord, size, size)
+
+    ctx.font = "18px dang"
+    ctx.fillStyle = "#fafbf9"
+    ctx.textAlign = 'right'
+    ctx.textBaseline = 'alphabetic'
+
+    segmentToUse = ""
+
+    if(username.length > 19){
+        username = username.slice(0, 18) + ".."
+    }
+    if(frameIndex > 2){
+        const segments = username.length / 6 // frames 3-8
+    
+        const charsToUse = Math.round(segments * (frameIndex-2))
+        const currentSegment = username.slice(0,charsToUse).padEnd(username.length, " ")
+        segmentToUse = currentSegment
+    }
+
+    const textXAxis = 141
+    const textYAxis = 222
+    const textWidth = 141
+    ctx.fillText(segmentToUse.slice(0,20), textXAxis, textYAxis, textWidth)
+    
+    return ctx
+}
+
+/**
+ * The main function of the gifWorker
+ * 1. It extracts the gif via dissectGif()
+ * 2. We set the encoder via GIFEncoder
+ * 3. We make an empty gifChunks array and we create a readStream, and for every time it's called we push its buffer into gifChunks array
+ * 4. We start the encoder and set its options
+ * 5. Then we load the game into canvas and we create a new canvas and ctx
+ * 6. We start a for-loop for each frame, where it's edited and built by buildEditedImage() then we add it into encoder
+ * 7. Once we are done we call encoder.finish()
+ * 8. We use Buffer.concat() to turn the gifChunks Array into a full buffer and then we return that full buffer
+ * @returns {Uint8Array} - The Uint8Array Buffer of the newly built gif from the parameters given.
+*/
 async function fullProcess({avatarPath, username}){
 
-    const extractedFrames = await dissectGif()
-    const fontPath2 = path.join(process.cwd(), "media/pixel-danganronpa.otf")
-    GlobalFonts.registerFromPath(fontPath2, "dang")
-    const canvas = createCanvas(width, height)
-    const ctx = canvas.getContext("2d")
-
-    const encoder = new GIFEncoder(width, height, 'neuquant', true)
-    // const writeStream = createWriteStream('./output_1_.gif')
-    // encoder.createReadStream().pipe(writeStream)
-
+    const extractedBackgroundImages = await dissectGif()
+    
+    const encoder = new GIFEncoder(width, height, 'octree', true)
+    
     const gifChunks = [];
     const encoderStream = encoder.createReadStream()
-
     encoderStream.on("data", (chunk)=> gifChunks.push(chunk))
-
+    
+    // Encoder start & options
     encoder.start()
     encoder.setRepeat(0)
     encoder.setDelay(100)
     encoder.setQuality(10)
+    
+    const avatarImage = await loadImage(avatarPath)
+    
+    const canvas = createCanvas(width, height)
+    const ctx = canvas.getContext("2d")
 
-    const avatar = await loadImage(avatarPath)
+    for (let i = 0; i < extractedBackgroundImages.length; i++){
+        const frame = await buildEditedImage(
+            ctx,
+            extractedBackgroundImages[i],
+            avatarImage,
+            i,
+            username
+        );
 
-    const frameBuffers = await Promise.all(extractedFrames.map(frame=>streamToBuffer(frame.getImage())))
-    // console.log(frameBuffers)
-    for (let currentFrameIndex = 0; currentFrameIndex < 51; currentFrameIndex++){
-
-        const img = await loadImage(frameBuffers[currentFrameIndex])
-
-        ctx.clearRect(0,0, width, height)
-        const frameData = await buildStudio(ctx, img,avatar, currentFrameIndex, username)
-        encoder.addFrame(frameData)
+        encoder.addFrame((frame))
     }
+
     encoder.finish()
     const finalBuffer = Buffer.concat(gifChunks)
     
     return finalBuffer
 }
 
-async function buildStudio(ctx,bg,avatar, frameNum, username){
-    const cordsArray = imgDir(frameNum)
-    const xCord = cordsArray[0]
-    const yCord = cordsArray[1]
-    const size = 90
-
-    
-    ctx.drawImage(bg, 0,0, width,height)
-    ctx.drawImage(avatar, xCord, yCord, size, size)
-
-    ctx.font = "18px dang"
-    ctx.fillStyle = "#fafbf9"
-    ctx.textAlign = 'right'
-    ctx.textBaseline = 'alphabetic'
-    // const username = "CHIAKI NANAMI DESU"
-    segmentToUse = ""
-    if(username.length > 19){
-        username = username.slice(0, 18) + ".."
-    }
-    if(frameNum > 2){
-        const segments = username.length / 6 // frames 3-8
-    
-        const charsToUse = Math.round(segments * (frameNum-2))
-        const currentSegment = username.slice(0,charsToUse).padEnd(username.length, " ")
-        segmentToUse = currentSegment
-    }
-
-    ctx.fillText(segmentToUse.slice(0,20), 141, 222, 141)
-    
-    return ctx.getImageData(0,0, width, height).data
-}
 
 module.exports = fullProcess;
