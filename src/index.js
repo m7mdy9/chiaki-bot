21580d7662afacdd61713bead8168715d63fda0a
m7mdy9 <hamestar1122@gmail.com>
2026-06-20 22:42:09 +0300

    Documented index.js and removed an unnecessary variable.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ require("dotenv").config({quiet:true});
 const { Client, GatewayIntentBits, Collection} = require('discord.js');
 const runEventHandler = require("./handlers/eventHandler")
 const { execSync } = require("child_process");
-const { error } = require("console");
 
+// checking current branch to see whether we will use the main client id and token or the testing id and token
 let currentBranch = "main"
 try {
     currentBranch = execSync("git branch --show-current").toString().trim()
@@ -11,16 +11,18 @@ try {
     console.error("Couldn't detect branch, auto set to main.", err)
 }
 
+// Correcting the console color codes, as they are usually broken due to the ways .env saves values
 ['RED', 'YELLOW', 'GREEN', 'RESET', 'DARK_GREY'].forEach(key => {
     if (process.env[key]) {
         process.env[key] = process.env[key].replace(/\\x1b/g, '\x1b');
     }
 });
 
+// Assigning token and clientId based on whether the current branch is main or not
 const botToken = currentBranch == "main" ? process.env.TOKEN : process.env.TESTING_TOKEN
 const clientId = currentBranch == "main" ? process.env.clientId : process.env.TESTING_clientId
 
-const ownerId = process.env.ownerId;
+// creating our client with our needed intents and initializing an empty discordjs Collection to save our commands in, via the commandHandler.js 
 const client = new Client({ intents: [GatewayIntentBits.GuildMembers,GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 client.commands = new Collection();
 client.clientId = clientId
